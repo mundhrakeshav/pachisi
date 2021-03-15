000ebb8d6044e64ad3e40f390425f3c73c5371b5
Keshav Mundhra <mundhrakeshav11@gmail.com>
2021-03-15 21:44:26 +0530

    Crypto card UI complete

--- pachisi-frontend/src/components/PriceFeeds/CryptoPriceFeedsUi/cards/ethPairCard.jsx
+++ pachisi-frontend/src/components/PriceFeeds/CryptoPriceFeedsUi/cards/ethPairCard.jsx
@@ -7,37 +7,134 @@ import {
   Button,
   Dropdown,
   DropdownButton,
+  InputGroup,
+  FormControl,
 } from "react-bootstrap";
 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 const EthPairCard = () => {
-  const { selectedETHPairPriceTag } = useContext(CryptoPriceFeedsPageContext);
+  const {
+    ETHPairAssets,
+    selectedETHPair,
+    selectedETHPairPriceTag,
+  } = useContext(CryptoPriceFeedsPageContext);
+  const [date, setStartDate] = useState(new Date());
+
   return (
-    <Row className="price-feeds-right-ui-wrapper">
-      <Col>
-        <Card className="price-feeds-card">
-          <Card.Body>
-            <Card.Title>USD Pair</Card.Title>
-            <Card.Subtitle>
-              This is price pair of all the listed assets with USD.
-            </Card.Subtitle>
-            <br />
-            <Row>
-              {" "}
-              <Col>
-                <DropDownButtonETHpair />
-              </Col>
-              <Col>
-                <b>{selectedETHPairPriceTag + " ETH"}</b>
-              </Col>
-            </Row>{" "}
-            <br />
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
+    <div className="pair-card-wrapper">
+      <Row className="price-feeds-right-ui-wrapper">
+        <Col>
+          <Card className="price-feeds-card">
+            <Card.Body>
+              <Card.Title className="price-feeds-card-title">
+                ETH Pair
+              </Card.Title>
+              <Card.Subtitle className="price-feeds-card-subtitle">
+                This is price pair of all the listed assets with ETH.
+              </Card.Subtitle>
+              <br />
+              <Row>
+                <Col>
+                  <DropDownButtonETHpair />
+                </Col>
+                <Col>
+                  <b>{"Current Price: " + selectedETHPairPriceTag + " ETH"}</b>
+                </Col>
+              </Row>
+              <br />
+
+              <Row>
+                <Col xs="5" className="date-picker">
+                  <b>
+                    {`Price of ${ETHPairAssets[selectedETHPair]["name"]} on`}
+                    <DatePicker
+                      selected={date}
+                      onChange={(_date) => setStartDate(_date)}
+                      minDate={new Date()}
+                      placeholderText="Select a date."
+                      dateFormat="dd/MM/yyyy"
+                    />{" "}
+                    is
+                  </b>
+                </Col>
+                <Col xs="2">
+                  <SymbolDropDown date={date} />
+                </Col>
+                <Col xs="5">
+                  <InputGroup className="mb-3">
+                    <FormControl
+                      placeholder="Predicted Price"
+                      aria-label="Amount (to the nearest dollar)"
+                      type="number"
+                    />
+                    <InputGroup.Append>
+                      <InputGroup.Text>ETH</InputGroup.Text>
+                    </InputGroup.Append>
+                  </InputGroup>
+                </Col>
+              </Row>
+              <br />
+              <Row>
+                <Col>
+                  <InputGroup className="mb-3">
+                    <FormControl
+                      placeholder="Initial bet should be greater than 20 DAI."
+                      aria-label="Amount (to the nearest dollar)"
+                      type="number"
+                    />
+                    <InputGroup.Append>
+                      <InputGroup.Text>DAI</InputGroup.Text>
+                    </InputGroup.Append>
+                  </InputGroup>
+                </Col>
+                <Col>
+                  <Button
+                    style={{
+                      borderRadius: "20px",
+                      width: "20vw",
+                      height: "5vh",
+                      backgroundColor: "#340068",
+                      border: "1px solid #340068",
+                      boxShadow: "10px 10px 8px #888888",
+                    }}>
+                    Create Market
+                  </Button>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 };
+
+const SymbolDropDown = (props) => {
+  const [selectedSymbol, setSelectedSymbol] = useState("<");
+  const symbols = [">", "<"];
+
+  const dropDownSymbolList = symbols.map((symbol, index) => {
+    return (
+      <Dropdown.Item
+        key={index}
+        eventKey={index}
+        onSelect={() => {
+          setSelectedSymbol(symbols[index]);
+        }}>
+        {symbol}
+      </Dropdown.Item>
+    );
+  });
+
+  return (
+    <DropdownButton id="dropdown-basic-button" title={selectedSymbol}>
+      {dropDownSymbolList}
+    </DropdownButton>
+  );
+};
+
 const DropDownButtonETHpair = () => {
   const { ETHPairAssets, selectedETHPair, changeETHPair } = useContext(
     CryptoPriceFeedsPageContext
--- pachisi-frontend/src/components/PriceFeeds/CryptoPriceFeedsUi/cards/usdPairCard.jsx
+++ pachisi-frontend/src/components/PriceFeeds/CryptoPriceFeedsUi/cards/usdPairCard.jsx
@@ -8,35 +8,130 @@ import {
   Button,
   Dropdown,
   DropdownButton,
+  InputGroup,
+  FormControl,
 } from "react-bootstrap";
 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 const UsdPairCard = () => {
-  const { selectedUSDPairPriceTag } = useContext(CryptoPriceFeedsPageContext);
+  const {
+    selectedUSDPairPriceTag,
+    USDPairAssets,
+    selectedUSDPair,
+  } = useContext(CryptoPriceFeedsPageContext);
+  const [date, setStartDate] = useState(new Date());
+
   return (
-    <Row className="price-feeds-right-ui-wrapper">
-      <Col>
-        <Card className="price-feeds-card">
-          <Card.Body>
-            <Card.Title>USD Pair</Card.Title>
-            <Card.Subtitle>
-              This is price pair of all the listed assets with USD.
-            </Card.Subtitle>
-            <br />
-            <Row>
-              {" "}
-              <Col>
-                <DropDownButtonUSDpair />
-              </Col>
-              <Col>
-                <b>{selectedUSDPairPriceTag + " USD"}</b>
-              </Col>
-            </Row>{" "}
-            <br />
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
+    <div className="pair-card-wrapper">
+      <Row className="price-feeds-right-ui-wrapper">
+        <Col>
+          <Card className="price-feeds-card">
+            <Card.Body>
+              <Card.Title className="price-feeds-card-title">
+                USD Pair
+              </Card.Title>
+              <Card.Subtitle className="price-feeds-card-subtitle">
+                This is price pair of all the listed assets with USD.
+              </Card.Subtitle>
+              <br />
+              <Row>
+                <Col>
+                  <DropDownButtonUSDpair />
+                </Col>
+                <Col>
+                  <b>{"Current Price: " + selectedUSDPairPriceTag + " USD"}</b>
+                </Col>
+              </Row>
+              <br />
+              <Row>
+                <Col xs="5" className="date-picker">
+                  <b>
+                    {`Price of ${USDPairAssets[selectedUSDPair]["name"]} on `}
+                    <DatePicker
+                      selected={date}
+                      onChange={(_date) => setStartDate(_date)}
+                      minDate={new Date()}
+                      placeholderText="Select a date."
+                      dateFormat="dd/MM/yyyy"
+                    />
+                    {` is`}
+                  </b>
+                </Col>
+                <Col xs="2">
+                  <SymbolDropDown date={date} />
+                </Col>
+                <Col xs="5">
+                  <InputGroup className="mb-3">
+                    <FormControl
+                      placeholder="Predicted Price"
+                      aria-label="Amount (to the nearest dollar)"
+                      type="number"
+                    />
+                    <InputGroup.Append>
+                      <InputGroup.Text>USD</InputGroup.Text>
+                    </InputGroup.Append>
+                  </InputGroup>
+                </Col>
+              </Row>
+              <br />
+              <Row>
+                <Col>
+                  <InputGroup className="mb-3">
+                    <FormControl
+                      placeholder="Initial bet should be greater than 20 DAI."
+                      aria-label="Amount (to the nearest dollar)"
+                      type="number"
+                    />
+                    <InputGroup.Append>
+                      <InputGroup.Text>DAI</InputGroup.Text>
+                    </InputGroup.Append>
+                  </InputGroup>
+                </Col>
+                <Col>
+                  <Button
+                    style={{
+                      borderRadius: "20px",
+                      width: "20vw",
+                      height: "5vh",
+                      backgroundColor: "#340068",
+                      border: "1px solid #340068",
+                      boxShadow: "10px 10px 8px #888888",
+                    }}>
+                    Create Market
+                  </Button>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+const SymbolDropDown = (props) => {
+  const [selectedSymbol, setSelectedSymbol] = useState("<");
+  const symbols = [">", "<"];
+
+  const dropDownSymbolList = symbols.map((symbol, index) => {
+    return (
+      <Dropdown.Item
+        key={index}
+        eventKey={index}
+        onSelect={() => {
+          setSelectedSymbol(symbols[index]);
+        }}>
+        {symbol}
+      </Dropdown.Item>
+    );
+  });
+
+  return (
+    <DropdownButton id="dropdown-basic-button" title={selectedSymbol}>
+      {dropDownSymbolList}
+    </DropdownButton>
   );
 };
 
@@ -48,8 +143,6 @@ const DropDownButtonUSDpair = () => {
     changeUSDPair,
   } = useContext(CryptoPriceFeedsPageContext);
   const dropDownItemList = USDPairAssets.map((USDPairAsset, index) => {
-    console.log(USDPairAsset);
-
     return (
       <Dropdown.Item
         key={index}
@@ -66,16 +159,6 @@ const DropDownButtonUSDpair = () => {
     <DropdownButton
       id="dropdown-basic-button"
       title={USDPairAssets[selectedUSDPair]["name"]}>
-      {/* <Dropdown.Item
-        eventKey="1"
-        onSelect={(eventKey) => {
-          console.log(eventKey);
-        }}>
-        Action
-      </Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item> */}
-
       {dropDownItemList}
     </DropdownButton>
   );
