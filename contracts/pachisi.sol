68cab299031b14ddcdb71aed2f0f86c473e71e34
Keshav Mundhra <mundhrakeshav11@gmail.com>
2021-03-24 01:41:24 +0530

    Lottery Contracts

---
+++ contracts/pachisi.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.6.7;
+import {PachisiCryptoPrediction} from "./CryptoPrediction.sol";
+import {LotteryPrediction} from "./lotteryPrediction.sol";
+
+contract Pachisi is PachisiCryptoPrediction, LotteryPrediction {
+    
+    
+}
