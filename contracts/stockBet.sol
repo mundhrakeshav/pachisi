af4f9d2720dfe2ffe88682c5e9eb3c99baace2b5
Keshav Mundhra <mundhrakeshav11@gmail.com>
2021-03-27 05:38:02 +0530

    stocks contracts

--- contracts/stockBet.sol
+++ contracts/stockBet.sol
@@ -8,16 +8,13 @@ import "./ShareToken.sol";
 
 contract StockBet is ChainlinkClient, Ownable {
     
-    uint public betResolveTime;
-    
-    uint public resolvedPrice;
-    
     address private oracle;
     bytes32 private getCallJobId;
     bytes32 private alamClockJobId;
     uint256 private fee;
     
     string public url;
+    string public betStock;
     
     bool public betResolved;
     bool public alarmClockResolved;
@@ -29,6 +26,8 @@ contract StockBet is ChainlinkClient, Ownable {
     bytes public constant greaterSymbol = bytes(">"); //greateThan or lessThan
     bytes public constant lessSymbol = bytes("<"); //greaterThan or lessThan
     
+    uint public betResolveTime;
+    uint public resolvedPrice;
     uint public totalPriceOfRemainingTokens;
     uint public predictionPrice; //price is greaterThan/lessThan predictionPrice
     uint public volume;
@@ -47,7 +46,7 @@ contract StockBet is ChainlinkClient, Ownable {
 //      Oracle address: 0x2f90A6D021db21e1B2A077c5a37B3C7E75D15b7e
 //      JobID: a7ab70d561d34eb49e9b1612fd2e044b
      
-    constructor(string memory _url, uint _betResolveTime) public {
+    constructor(string memory _url, uint _betResolveTime, string memory _betStock, uint _predictionPrice, string memory _betSymbol) public {
         setPublicChainlinkToken();
         oracle = 0x2f90A6D021db21e1B2A077c5a37B3C7E75D15b7e;
         getCallJobId = "29fa9aa13bf1468788b7cc4a500a45b8";
@@ -55,6 +54,20 @@ contract StockBet is ChainlinkClient, Ownable {
         fee = 0.1 * 10 ** 18; // 0.1 LINK
         url = _url;
         betResolveTime = _betResolveTime;
+        betStock = _betStock;
+        predictionPrice = _predictionPrice;
+        symbol = bytes(_betSymbol);
+        
+        betResolved = false;
+        alarmClockResolved = false;
+        
+        trueToken = new ShareToken();
+        falseToken = new ShareToken();
+        trueToken.mint(address(this), 10**30);  //10^12 tokens considering decimal 18
+        falseToken.mint(address(this), 10**30); //10^12 tokens
+        
+        totalPriceOfRemainingTokens = 10**30;
+
     }
     
      function bet(uint _amount, bool _userBet, address _userAddress) public 
@@ -76,13 +89,13 @@ contract StockBet is ChainlinkClient, Ownable {
     }
     
     function claimableFunds(address _userAddress) public view returns(uint){
-        if(keccak256(abi.encodePacked(symbol)) == keccak256(abi.encodePacked(greaterSymbol)) && predictionPrice > resolvedPrice) {
+        if(keccak256(abi.encodePacked(symbol)) == keccak256(abi.encodePacked(greaterSymbol)) && predictionPrice * 10e18 > resolvedPrice) {
             return userOwnedTrueTokens[_userAddress] * volume/trueTokensInMarket;
         } else {
             return userOwnedFalseTokens[_userAddress] * volume/falseTokensInMarket;    
         }
         
-        if(keccak256(abi.encodePacked(symbol)) == keccak256(abi.encodePacked(lessSymbol)) && predictionPrice < resolvedPrice) {
+        if(keccak256(abi.encodePacked(symbol)) == keccak256(abi.encodePacked(lessSymbol)) && predictionPrice * 10e18 < resolvedPrice) {
             return userOwnedTrueTokens[_userAddress] * volume/trueTokensInMarket;
         } else {
             return userOwnedFalseTokens[_userAddress] * volume/falseTokensInMarket;    
@@ -93,13 +106,13 @@ contract StockBet is ChainlinkClient, Ownable {
         hasUserClaimed[_userAddress] = true;
     }
     
-    function requestAlarmClock(uint _betResolveTime) public onlyOwner{
+    function requestAlarmClock() public onlyOwner{
         Chainlink.Request memory req = buildChainlinkRequest(alamClockJobId, address(this), this.requestVolumeData.selector);
-        req.addUint("until", _betResolveTime);
+        req.addUint("until", betResolveTime);
         sendChainlinkRequestTo(oracle, req, fee);
     }
     
-    /**
+    /**0xb113D314C4946B328a44efE177DC956B77094337,0x405D6C6551a91F0e9eEFfB7b9d0f731FeD3A061f
      * Create a Chainlink request to retrieve API response, find the target
      * data, then multiply by 1000000000000000000 (to remove decimal places from data).
      */
--- contracts/stockPrediction.sol
+++ contracts/stockPrediction.sol
@@ -1,4 +1,5 @@
 // https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=EHZZFEOMSTGL3I2V
+// 0xb113D314C4946B328a44efE177DC956B77094337,0x405D6C6551a91F0e9eEFfB7b9d0f731FeD3A061f,0xCacf67a8F62F46905967661de09C88Db252235DB,0xf2f19043Dda4c2a4231140e522BaA05D1d7bb744
 
 
 pragma solidity ^0.6.7;
@@ -17,24 +18,46 @@ contract PachisiStockPrediction is AaveClient, ERC20Client {
     function getStockBets() public view returns(address[] memory) {
         return stockBetAddresses;
     }    
-
+    
+    
+    function getURL(string memory _stock) public view returns(string memory) {
+        return string(abi.encodePacked("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=",_stock,"&apikey=EHZZFEOMSTGL3I2V"));
+    }    
+    
+// string memory _url, uint _betResolveTime, string memory _betStock, uint _predictionPrice, string memory _betSymbol
     function createStockBet(  uint _betResolveTime, 
-                            string memory _betToken, 
+                            string memory _betStock, 
                             string memory _symbol, 
                             uint _predictionPrice, 
                             uint _initialBetAmount, 
                             bool _userBet) public enoughLinkBalanceOnContract {
-        // StockBet _stockBet = StockBet();
-        // PachisiCryptoBet(address _aggregatorAddress, uint _betResolveTime, string memory _betPair, string memory _betToken, uint _predictionPrice, string memory _betSymbol)
-        // require(USDPairAggregatorAddress[_betToken] != address(0x0));
-        // PachisiCryptoBet _pachisiCryptoBet = new PachisiCryptoBet(USDPairAggregatorAddress[_betToken], _betResolveTime, "USD", _betToken, _predictionPrice, _symbol);
-        // _pachisiCryptoBet.bet(_initialBetAmount, _userBet, msg.sender);
-        // linkContract.transfer(address(_pachisiCryptoBet), 10**17);
-        // tokenUSDBetAddresses.push(address(_pachisiCryptoBet));
-        // _pachisiCryptoBet.requestAlarmClock(_betResolveTime);
-        // pullDai(msg.sender, _initialBetAmount);
-        // daiContract.approve(AAVE_LENDING_POOL_ADDRESS, _initialBetAmount);
-        // makeDepositToAave(daiContractAddress, _initialBetAmount, address(this)); 
+        StockBet _stockBet = new StockBet(getURL(_betStock), _betResolveTime, _betStock, _predictionPrice, _symbol);
+        _stockBet.bet(_initialBetAmount, _userBet, msg.sender);
+        linkContract.transfer(address(_stockBet), 2 * 10**17);
+        stockBetAddresses.push(address(_stockBet));
+        _stockBet.requestAlarmClock();
+        pullDai(msg.sender, _initialBetAmount);
+        daiContract.approve(AAVE_LENDING_POOL_ADDRESS, _initialBetAmount);
+        makeDepositToAave(daiContractAddress, _initialBetAmount, address(this)); 
+    }
+    
+    
+    function placeStockBet(address _betAddress, string memory _betStock, uint _betAmount, bool _userBet) public {
+        StockBet _stockBet = StockBet(_betAddress);
+        _stockBet.bet(_betAmount, _userBet, msg.sender);
+        userStockBets[msg.sender][_betStock].push(_betAddress);
+        pullDai(msg.sender, _betAmount);
+        daiContract.approve(AAVE_LENDING_POOL_ADDRESS, _betAmount);
+        makeDepositToAave(daiContractAddress, _betAmount, address(this)); 
+    }
+    
+    function claimStockFunds(address _betAddress) public {
+        StockBet _stockBet = StockBet(_betAddress);
+        require(_stockBet.betResolved(),"Bet hasn't been resolved.");
+        require(!_stockBet.hasUserClaimed(msg.sender),"Funds have been claimed.");
+        uint _totalFundsToBeClaimed = _stockBet.claimableFunds(msg.sender);
+        _stockBet.claimFunds(msg.sender);
+        withdrawFromAave(daiContractAddress, _totalFundsToBeClaimed, msg.sender);
     }
     
 
